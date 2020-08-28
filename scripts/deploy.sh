62a2889414fda8296f3e1b87105780cac0163ca8
BWDaniels97 <bwdaniels97@gmail.com>
2020-08-28 14:21:17 +0000

    jenkins

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 ssh jenkins@35.189.107.29 << EOF
-cd QA-SFIA-2/
 export DATABASE_URI=mysql+pymysql://root:1234@34.105.158.49/project2db
 export SECRET_KEY=aujdhfouisnbfdbsedohgosiehdjgoshedpgnsdoia
 docker stack deploy --compose-file docker-compose.yaml application
