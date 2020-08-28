c28c2153f69ef2400bec4e769fdaf943e858187b
BWDaniels97 <bwdaniels97@gmail.com>
2020-08-28 14:28:13 +0000

    jenkins

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 ssh jenkins@35.189.107.29 << EOF
 git clone https://github.com/BWDaniels97/QA-SFIA-2.git
 cd QA-SFIA-2/
+git pull origin ansible
 export DATABASE_URI=mysql+pymysql://root:1234@34.105.158.49/project2db
 export SECRET_KEY=aujdhfouisnbfdbsedohgosiehdjgoshedpgnsdoia
 docker stack deploy --compose-file docker-compose.yaml application
