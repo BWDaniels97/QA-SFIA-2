74fff8c074599546031a8a69da0f506b532f70b9
BWDaniels97 <bwdaniels97@gmail.com>
2020-08-30 21:23:37 +0000

    hidden varibles

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -3,10 +3,10 @@ sudo apt-get update
 sudo apt update && sudo apt install -y python3 python3-pip python3-venv
 pip3 install -r service1/requirements.txt
 
-export DATABASE_URI=mysql+pymysql://root:1234@34.105.158.49/project2db
-export SECRET_KEY=aujdhfouisnbfdbsedohgosiehdjgoshedpgnsdoia
-export TEST_DB_URI=mysql+pymysql://root:1234@34.105.158.49/test_project2db
-export TEST_SECRET_KEY=aujdhfouisnbfdbsedohgosiehdjgoshedpgnsdoia
+export DATABASE_URI=${DATABASE_URI}
+export SECRET_KEY=${SECRET_KEY}
+export TEST_DB_URI=${TEST_DB_URI}
+export TEST_SECRET_KEY=${TEST_SECRET_KEY}
 
 cd service1
 pytest --cov application
